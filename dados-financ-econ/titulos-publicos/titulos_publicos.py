15d9ea6b61c1218a0850008ea3be38f19b05d7c1
Vitor Saito <vitorsaito95@gmail.com>
2023-09-11 14:33:04 -0300

    adicinando o scraper que faz o download das planilhas dos títulos públicos

---
+++ dados-financ-econ/titulos-publicos/titulos_publicos.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from time import sleep
+
+class WebScrapingTitulosPublicos:
+    def __init__(self):
+
+        # Diretório do download do arquivo
+        self.download_directory = 'C://Users//vitor//projetos_python//python_b3//web-scraping//dados-financ-econ//titulos-publicos//dados'
+
+    def download_arquivo(self, url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            nome_arquivo = os.path.join(self.download_directory, os.path.basename(url))
+            with open(nome_arquivo, 'wb') as f:
+                f.write(response.content)
+
+    def loop_download_arquivo(self):
+        # Lista com as URLs dos downloads dos arquivos. Esses links são do site https://www.tesourodireto.com.br/titulos/historico-de-precos-e-taxas.htm
+        links = [
+            'https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2023/NTN-B_2023.xls',
+            'https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2023/NTN-B_Principal_2023.xls',
+            'https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2023/LTN_2023.xls',
+        ]
+
+        for link in links:
+            self.download_arquivo(link)
+            sleep(5)  # Intervalo entre downloads
+
+
+def main():
+    titulos_publicos = WebScrapingTitulosPublicos()
+    titulos_publicos.loop_download_arquivo()
+
+
+if __name__ == "__main__":
+    main()
