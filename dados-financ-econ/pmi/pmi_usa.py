f20e557c6f886046a4f9afb148e4c87e0b71ac63
Vitor Saito <vitorsaito95@gmail.com>
2023-09-22 17:20:04 -0300

    arrumei algumas linhas que estavam erradas

--- dados-financ-econ/pmi/pmi_usa.py
+++ dados-financ-econ/pmi/pmi_usa.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
