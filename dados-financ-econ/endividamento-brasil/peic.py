d2d4c4c72e1ada1fef49c6bfc7dff0bfa92a63ee
Vitor Saito <vitorsaito95@gmail.com>
2023-10-15 21:42:02 -0300

    adicionei a função que renomeia o arquivo baixado

--- dados-financ-econ/endividamento-brasil/peic.py
+++ dados-financ-econ/endividamento-brasil/peic.py
@@ -4,18 +4,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+import os
+import glob
+import re
 
 
 class PesquisaPeic:
     def __init__(self):
         # Diretório do download do arquivo 
-        self.path_download = r"C:\Users\vitor\projetos_python\python_b3\web-scraping\dados-financ-econ\endividamento_br"
+        self.path_download = r'C:\Users\vitor\projetos_python\python_b3\web-scraping\dados-financ-econ\endividamento-brasil'
 
         options = Options()
-        options.set_preference("browser.download.folderList", 2)  
-        options.set_preference("browser.download.dir", self.path_download)
-        options.set_preference("browser.download.useDownloadDir", True) 
-        options.set_preference("browser.download.viewableInternally.enabledTypes", "")  
+        options.set_preference('browser.download.folderList', 2)  
+        options.set_preference('browser.download.dir', self.path_download)
+        options.set_preference('browser.download.useDownloadDir', True) 
+        options.set_preference('browser.download.viewableInternally.enabledTypes', '')  
         self.driver = webdriver.Firefox(options=options)
 
     def acessar_site(self):
@@ -30,6 +33,21 @@ class PesquisaPeic:
         ))
         botao.click()
 
+    def renomear_arquivo(self):
+        # Selecionando apenas o arquivo excel
+        padrao = '*.xlsx'
+        nome_arquivos = glob.glob(os.path.join(self.path_download, padrao))
+
+        # Renomeando o arquivo do formato "peic_link_download_202309.xlsx" para "peic_202309.xlsx"
+        if nome_arquivos:
+            nome_arquivo = os.path.basename(nome_arquivos[0])
+            novo_nome = re.sub(r'peic_link_download_', 'peic_', nome_arquivo)
+            novo_caminho = os.path.join(self.path_download, novo_nome)
+            os.rename(nome_arquivos[0], novo_caminho)
+            
+        else:
+            print('Nenhum arquivo .xlsx encontrado na pasta.')
+
     def fechar_site(self):
         # Fechando o driver
         self.driver.quit()
@@ -39,6 +57,7 @@ def main():
     peic = PesquisaPeic()
     peic.acessar_site()
     peic.botao_downlaod()
+    peic.renomear_arquivo()
     peic.fechar_site()
 
 
