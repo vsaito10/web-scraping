58a35654d2240bdcf3237af9138807c25cca7831
Vitor Saito <vitorsaito95@gmail.com>
2024-02-23 14:13:29 -0300

    atualizando o código para pegar automaticamente o link da planilha mais atual

--- dados-financ-econ/proxy-rate/proxy_rate.py
+++ dados-financ-econ/proxy-rate/proxy_rate.py
@@ -1,13 +1,13 @@
-import os
 import requests
+from bs4 import BeautifulSoup
+import re
+import os
+
 
 class WebScrapingProxyRate:
     def __init__(self):
         # Diretório do download do arquivo
         self.download_directory = 'C://Users//vitor//projetos_python//python_b3\web-scraping//dados-financ-econ//proxy-rate'
-        
-        # URL do download do arquivo. Esse link é do site https://www.frbsf.org/economic-research/indicators-data/proxy-funds-rate/
-        self.url = 'https://www.frbsf.org/wp-content/uploads/sites/4/proxy-funds-rate-data.xlsx?202305052'
 
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
@@ -17,13 +17,31 @@ class WebScrapingProxyRate:
             'Connection': 'close'
         }
 
+        # Site do FED San Francisco
+        url = 'https://www.frbsf.org/research-and-insights/data-and-indicators/proxy-funds-rate/'
+
+        # Solicitando o HTTP GET do site do FED San Francisco
+        response = requests.get(url, headers=self.headers)
+
+        # HTML da página
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Usando o regex para encontrar o link da planilha
+        link_pattern = re.compile(r'/wp-content/uploads/proxy-funds-rate-data\.xlsx\?(\d+)')
+        link = soup.find('a', href=link_pattern)
+        # Selecionando o href -> '/wp-content/uploads/proxy-funds-rate-data.xlsx?20240105'
+        href = link.get('href')
+
+        # URL do download da planilha do Proxy Rate
+        self.url_planilha = f'https://www.frbsf.org/{href}'
+
     def baixar_arquivo(self):
-        response = requests.get(self.url, headers=self.headers)
-        if response.status_code == 200:
-            nome_arquivo = os.path.basename(self.url).split("?")[0]  # Obtém o nome do arquivo sem a parte da URL após o "?" -> 'proxy-funds-rate-data.xlsx'
+        response_planilha = requests.get(self.url_planilha, headers=self.headers)
+        if response_planilha.status_code == 200:
+            nome_arquivo = os.path.basename(self.url_planilha).split("?")[0]  # Obtém o nome do arquivo sem a parte da URL após o "?" -> 'proxy-funds-rate-data.xlsx'
             nome_arquivo = os.path.join(self.download_directory, nome_arquivo)
             with open(nome_arquivo, 'wb') as f:
-                f.write(response.content)
+                f.write(response_planilha.content)
 
 
 def main():
