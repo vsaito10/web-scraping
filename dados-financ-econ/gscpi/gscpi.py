5c09b9fa8031937093afab3b44609efabff8d133
Vitor Saito <vitorsaito95@gmail.com>
2024-09-04 11:39:48 -0300

    arrumei pequenos erros no código

--- dados-financ-econ/gscpi/gscpi.py
+++ dados-financ-econ/gscpi/gscpi.py
@@ -9,14 +9,13 @@ import pandas as pd
 # 'Network' -> 'Fetch/XHR' -> 'gscpi_interactive_data.csv' -> 'Copy as cURL (cdm)' -> Insomnia
 url = 'https://www.newyorkfed.org/medialibrary/research/interactives/data/gscpi/gscpi_interactive_data.csv'
 
-payload = ""
 headers = {
     'cookie': 'shell%23lang=en; sxa_site=nyfrbpublic',
     'accept': 'application/json, text/plain, */*',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
 }
 
-response = requests.request('GET', url, data=payload, headers=headers)
+response = requests.request('GET', url, headers=headers)
 
 gscpi_table = response.text
 
@@ -27,4 +26,4 @@ df = pd.read_csv(
 )
 
 # Transformando o df em um arquivo csv
-df.to_csv('C://Users//vitor//projetos_python//python_b3//web-scraping//dados-financ-econ//gscpi//gscpi.csv')
+df.to_csv('C://Users//vitor//projetos_python//python_b3//web-scraping//dados-financ-econ//gscpi//gscpi.csv', sep=';')
