14b3cfef2b55260a2d2fedf97ebc6c11959ca168
Vitor Saito <vitorsaito95@gmail.com>
2024-09-04 11:38:35 -0300

    arrumei alguns erros pequenos no código

--- dados-financ-econ/atas-copom/ata_pdf_selenium.py
+++ dados-financ-econ/atas-copom/ata_pdf_selenium.py
@@ -2,24 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from time import sleep
-import os
-import re
-import requests
-import logging
+
 
 class AtasCopom:
     def __init__(self):
         # Diretório do download do arquivo 
-        self.path_download = r"C:\Users\vitor\projetos_python\python_b3\historico-arquivos\minutes-pdf"
+        self.path_download = r'C:\Users\vitor\projetos_python\python_b3\historico-arquivos\minutes-pdf'
 
         options = Options()
         #options.add_argument("--headless")
-        options.set_preference("browser.download.folderList", 2)  
-        options.set_preference("browser.download.dir", self.path_download)
-        options.set_preference("browser.download.useDownloadDir", True) 
-        options.set_preference("browser.download.viewableInternally.enabledTypes", "")  
-        options.set_preference("pdfjs.disabled", True)  
-        options.set_preference("plugin.disable_full_page_plugin_for_types", "application/pdf")  
+        options.set_preference('browser.download.folderList', 2)  
+        options.set_preference('browser.download.dir', self.path_download)
+        options.set_preference('browser.download.useDownloadDir', True) 
+        options.set_preference('browser.download.viewableInternally.enabledTypes', '')  
+        options.set_preference('pdfjs.disabled', True)  
+        options.set_preference('plugin.disable_full_page_plugin_for_types', 'application/pdf')  
 
         self.driver = webdriver.Firefox(options=options)
 
@@ -35,10 +32,10 @@ class AtasCopom:
         # Clicando no botão de download
         botao_download = self.driver.find_element(By.XPATH, '//*[@id="publicacao"]/div[1]/div/div/div/div[1]/div[2]/download/div/div/a')
         # Usando JavaScript para acionar o download diretamente
-        self.driver.execute_script("arguments[0].click();", botao_download)                            
+        self.driver.execute_script('arguments[0].click();', botao_download)                            
         sleep(10)            
 
-        print("Arquivo baixado com sucesso!")
+        print('Arquivo baixado com sucesso!')
         
     def fechar_site(self):
         # Fechando o driver
@@ -47,10 +44,10 @@ class AtasCopom:
 
 def main():
     atas = AtasCopom()
-    atas.acessar_site(url_path='https://www.bcb.gov.br/en/publications/copomminutes/minutes223')
+    atas.acessar_site(url_path='https://www.bcb.gov.br/en/publications/copomminutes/31072024')
     atas.fechar_site()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
